7dc15acac245b49009a5423a080083266b27b04c
Sohel Shaikh <104219521+Sohel-222@users.noreply.github.com>
2025-05-26 12:48:33 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,18 +1,15 @@
-const request = require('supertest');
-
-const app = require('../index.js'); // Adjust if your app filename is app.js
-
-
-describe('GET /nonexistentpage', function() {
-
-  it('should respond with 404 Not Found', function(done) {
-
-    request(app)
-
-      .get('/nonexistentpage')
-
-      .expect(404, done); // This is a cleaner way to end test if only checking status
-
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('./app');
+const expect = chai.expect;
+chai.use(chaiHttp);
+describe('Server', () => {
+ it('responds with status 200', done => {
+    chai.request(server)
+      .get('/')
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        done();
+      });
   });
-
 });
